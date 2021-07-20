791288db4ddea83bd572cd041bb33ee539c04bc2
Michał <64414824+MichalFurman@users.noreply.github.com>
2021-07-20 22:55:04 +0200

    Update new version

--- dataDb.php
+++ dataDb.php
@@ -8,18 +8,32 @@
 	{
 		
 		private $dbaccess;
+		private $table = null;
 		private $data = array();
 		private $commit = true;
 		private $log_rec = true;
 
-		function __construct(object $dbaccess, $commit=true, $log_rec=true) 
+		function __construct(object $dbaccess, string $table, $commit=true, $log_rec=true) 
 		{
-      			if (!isset($dbaccess)) throw new InvalidArgumentException('Missing idatabase class - please check arguments of dependency injection.');
+			if (!isset($dbaccess)) throw new InvalidArgumentException('Missing idatabase class - please check arguments of dependency injection.');
+			if (!isset($table)) throw new InvalidArgumentException('Missing database table - please check arguments of dependency injection.');
 			$this->dbaccess = $dbaccess;
+			$this->table = $table;
 			$this->commit = $commit;
 			$this->log_rec = $log_rec;
 		}
+
+		public function set_table(string $table=null)
+		{
+			if (!isset($table)) throw new InvalidArgumentException('Missing table parameter - please check arguments of method "set_table".');
+			$this->table = $table;
+		}
 		
+		public function get_table()
+		{
+			return $this->table;
+		}
+
 		public function set_commit(bool $flag=true)
 		{
 			$this->commit = $flag;
@@ -55,14 +69,13 @@
 
 		public function get($name=null, $index=0)
 		{		
-			$dimm = count($this->data);
 			if ($name !== null && !is_array($name)) {
 				if (isset($this->data[$index][$name])) return $this->data[$index][$name];
 				else if (isset($this->data[$name])) return $this->data[$name];
 				else return null;
 			}
 			else if ($name !== null && is_array($name)) {
-				if ($dimm > 1) return $this->dbaccess->flat_array($this->data, $name);
+				if (count($this->data) > 1) return $this->dbaccess->flat_array($this->data, $name);
 				else {
 					$result = array();
 					foreach ($name as $key => $value) {
@@ -120,30 +133,30 @@
 			$this->dbaccess->unlock_tables();
 		}
 		
-		protected function insert(string $table, bool $parse=true) :int
+		protected function insert(bool $parse=true) :int
 		{
 			if (isset($this->data[0]) && is_array($this->data[0])) {
 				$last_id = array();
 				foreach ($this->data as $row) {
-					$this->dbaccess->lock_tables($table, 'WRITE');
-					$last_id[] = $this->dbaccess->insert($table, $row, $parse, $this->commit, $this->log_rec);
+					$this->dbaccess->lock_tables($this->table, 'WRITE');
+					$last_id[] = $this->dbaccess->insert($this->table, $row, $parse, $this->commit, $this->log_rec);
 				}
 			} 
 			else {
-				$this->dbaccess->lock_tables($table, 'WRITE');
-				$last_id = $this->dbaccess->insert($table, $this->data, $parse, $this->commit, $this->log_rec);
+				$this->dbaccess->lock_tables($this->table, 'WRITE');
+				$last_id = $this->dbaccess->insert($this->table, $this->data, $parse, $this->commit, $this->log_rec);
 			}
 			return $last_id;
 		}
 
-		protected function update(string $table, int $id, bool $parse=true) :object
+		protected function update(int $id, bool $parse=true) :object
 		{
-			$this->dbaccess->lock_tables($table, 'WRITE');
-			$this->dbaccess->update($table, $this->data, $id, $parse, $this->commit, $this->log_rec);
+			$this->dbaccess->lock_tables($this->table, 'WRITE');
+			$this->dbaccess->update($this->table, $this->data, $id, $parse, $this->commit, $this->log_rec);
 			return $this;
 		}
 
-		protected function update_where(string $table, string $where, bool $parse=true) :object
+		protected function update_where(string $where, bool $parse=true) :object
 		{
 			$this->dbaccess->lock_tables($table, 'WRITE');
 			$this->dbaccess->update_where($table, $this->data, $where, $parse, $this->commit, $this->log_rec);
@@ -157,35 +170,36 @@
 			return $this;
 		}
 
-		protected function read(string $table, $select, $where=null, $order=null, $limit=null) :object
+		protected function read($select, $where=null, $order=null, $limit=null) :object
 		{
 			$this->reset();
-			$this->dbaccess->lock_tables($table, 'WRITE');
-			$this->data = $this->dbaccess->select($table, $select, $where, $order, $limit, $this->commit);
+			$this->dbaccess->lock_tables($this->table, 'WRITE');
+			$this->data = $this->dbaccess->select($this->table, $select, $where, $order, $limit, $this->commit);
 			if (count($this->data) == 1) $this->data = $this->data[0];
 			return $this;
 		}
 
-		protected function read_upd(string $table, $select, $where=null, $order=null, $limit=null, $lock='FOR UPDATE') :object
+		protected function read_upd($select, $where=null, $order=null, $limit=null, $lock='FOR UPDATE') :object
 		{
 			$this->reset();
-			$this->dbaccess->lock_tables($table, 'WRITE');
-			$this->data = $this->dbaccess->select($table, $select, $where, $order, $limit, $this->commit,$lock);
+			$this->dbaccess->lock_tables($this->table, 'WRITE');
+			$this->data = $this->dbaccess->select($this->table, $select, $where, $order, $limit, $this->commit,$lock);
 			if (count($this->data) == 1) $this->data = $this->data[0];
 			return $this;
 		}
 
-		protected function read_share(string $table, $select, $where=null, $order=null, $limit=null, $lock='LOCK IN SHARE MODE') :object
+		protected function read_share($select, $where=null, $order=null, $limit=null, $lock='LOCK IN SHARE MODE') :object
 		{
 			$this->reset();
-			$this->dbaccess->lock_tables($table, 'WRITE');
-			$this->data = $this->dbaccess->select($table, $select, $where, $order, $limit, $this->commit, $lock);
+			$this->dbaccess->lock_tables($this->table, 'WRITE');
+			$this->data = $this->dbaccess->select($this->table, $select, $where, $order, $limit, $this->commit, $lock);
 			if (count($this->data) == 1) $this->data = $this->data[0];
 			return $this;
 		}
 
-		public function exist(string $table, $wpis=null, $where=null)
+		public function exist($wpis=null, $where=null, string $table = null)
 		{
+			$table === null ? $table = $this->table : null;
 			$this->dbaccess->lock_tables($table, 'WRITE');
 			return $this->dbaccess->exist($table, $wpis, $where, $this->commit);
 		}
